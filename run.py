0a86e2ae8c368444dc12b702dda57bf7fc6f2c95
Paul Bowden <paulbowden673@gmail.com>
2021-09-11 10:10:19 +0000

    added loop

--- run.py
+++ run.py
@@ -13,17 +13,24 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
-    """ 
-    Get sales data from user
     """
-    print('Please enter Sales Figures from the last Sales Day')
-    print('Sales figures should be 6 figures separated by commas')
-    print('Example: 10,20,30,40,50,60\n')
+    Get sales figures input from the user.
+    """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input('Enter you data here:')
-    
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -38,7 +45,8 @@ def validate_data(values):
             )
     except ValueError as e:
             print(f'Invalid Data: {e}, Please try again\n')
-
+            return False
     
+    return True
+data = get_sales_data()
 
-get_sales_data()
